6f366d19165e04a8ad6ebf2bc8d09addbccb95f4
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-11-04 00:16:20 -0300

    gsap-animations

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import './src/Sass/style.scss'
+import gsap from "gsap";
 import IMask from 'imask';
 
 const inputCep = document.querySelector(".input-cep");
@@ -86,3 +87,28 @@ function exibirDados(json) {
 function clearInput() {
     inputMask.value = "";
 }
+
+let tl = gsap.timeline()
+
+tl.fromTo(".logo",
+    { y: -50, opacity: 0 },
+    { y: 0, opacity: 1, ease: "power4.out", duration: .7 });
+
+tl.fromTo(".title",
+    { x: -80, opacity: 0 },
+    { x: 0, duration: .7, ease: "power1.out", opacity: 1 }), '-=0.5';
+
+tl.fromTo("form",
+    { y: -60, opacity: 0 },
+    { y: 0, duration: .7, ease: "power1.out", opacity: 1 }, '-=0.5');
+
+tl.fromTo("#linkedin img",
+    { y: -50, opacity: 0 },
+    { y: 0, opacity: 1, ease: "power", duration: .7 }, '-=0.5');
+
+tl.fromTo("#github img",
+    { y: -50, opacity: 0 },
+    { y: 0, opacity: 1, ease: "power", duration: .8 }, '-=0.5');
+
+
+
