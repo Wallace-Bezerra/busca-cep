c2ae1c6e10468c6a648fed6b7e94ba2fa380fcb4
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-11-04 19:45:06 -0300

    done

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import './src/Sass/style.scss'
-import gsap from "gsap";
+import { animation } from './Js/animation';
 import IMask from 'imask';
 
+animation();
 const inputCep = document.querySelector(".input-cep");
 const btnBuscar = document.querySelector(".search");
 const inputs = document.querySelectorAll(".input-get input");
 
 const inputMask = IMask(inputCep, {
-    mask: '00000-000',
-    lazy: true
+    mask: '00000-000'
 })
 
 btnBuscar.addEventListener("click", (event) => {
@@ -88,27 +88,7 @@ function clearInput() {
     inputMask.value = "";
 }
 
-let tl = gsap.timeline()
 
-tl.fromTo(".logo",
-    { y: -50, opacity: 0 },
-    { y: 0, opacity: 1, ease: "power4.out", duration: .7 });
-
-tl.fromTo(".title",
-    { x: -80, opacity: 0 },
-    { x: 0, duration: .7, ease: "power1.out", opacity: 1 }), '-=0.5';
-
-tl.fromTo("form",
-    { y: -60, opacity: 0 },
-    { y: 0, duration: .7, ease: "power1.out", opacity: 1 }, '-=0.5');
-
-tl.fromTo("#linkedin img",
-    { y: -50, opacity: 0 },
-    { y: 0, opacity: 1, ease: "power", duration: .7 }, '-=0.5');
-
-tl.fromTo("#github img",
-    { y: -50, opacity: 0 },
-    { y: 0, opacity: 1, ease: "power", duration: .8 }, '-=0.5');
 
 
 
